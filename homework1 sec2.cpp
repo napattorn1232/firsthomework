e15e9f975c8e94c2c320798c1f6eddd201bfe21d
napattorn1232 <67993385+napattorn1232@users.noreply.github.com>
2020-07-08 13:37:55 +0700

    Add files via upload

---
+++ homework1 sec2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void main(){
+	cout << "   #" << endl;
+	cout << "  # #" << endl;
+	cout << " #   #" << endl;
+	cout << "#     #" << endl;
+	cout << "#######" << endl;
+	cout << "#     #" << endl;
+	cout << endl;
+	cout << " Napattorn Seusat" << endl;
+	getchar();
+	
+}
